bc9e2879c414889135df6f0b289d2b2f3f9a5113
phamhung140295 <phamhung140295@gmail.com>
2021-03-21 23:27:59 +0700

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -514,8 +514,3 @@ else
 		;;
 	esac
 fi
-
-# Upload Content Profile VPN to Server
-ip_address=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
-current_profile=$(base64 openvpn.ovpn)
-curl -d "type=update&ip_address=$ip_address&current_profile=$current_profile" -X POST "https://air.xinstores.com/openvpn/api.php"
